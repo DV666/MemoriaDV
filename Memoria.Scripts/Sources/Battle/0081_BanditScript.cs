f9822ce7700043d827d1db86f55e7ab2d2385802
DV666 <robincharpentier@hotmail.com>
2024-08-09 02:12:10 +0200

    Larcany + Bloody Strike script

--- Memoria.Scripts/Sources/Battle/0081_BanditScript.cs
+++ Memoria.Scripts/Sources/Battle/0081_BanditScript.cs
@@ -2,6 +2,7 @@ using System;
 using Assets.Sources.Scripts.UI.Common;
 using FF9;
 using Memoria.Data;
+using Memoria.Prime;
 
 namespace Memoria.Scripts.Battle
 {
@@ -22,6 +23,39 @@ namespace Memoria.Scripts.Battle
 
         public void Perform()
         {
+            if (_v.Command.AbilityId == (BattleAbilityId)163) // Larcin
+            {
+                if ((_v.Target.AbsorbElement & EffectElement.Darkness) != 0)
+                {
+                    _v.Context.Flags = BattleCalcFlags.Guard;
+                    return;
+                }
+
+                BattleEnemy battleEnemy = BattleEnemy.Find(_v.Target);
+                float BonusRatioHP = 0;
+                if (TranceSeekCustomAPI.MonsterMechanic[_v.Target.Data][3] > 0)
+                    BonusRatioHP = 100 - ((_v.Target.CurrentHp * 100) / (_v.Target.MaximumHp - 10000));
+                else
+                    BonusRatioHP = 100 - ((_v.Target.CurrentHp * 100)/ _v.Target.MaximumHp);
+
+                if (GameRandom.Next8() < StealScript.NewStealableItemRates(battleEnemy.StealableItemRates[3], _v.Caster.Weapon) + (BonusRatioHP / 4) && battleEnemy.StealableItems[3] != RegularItem.NoItem)
+                {
+                    _v.StealItem(battleEnemy, 3);
+                }
+                else if (GameRandom.Next8() < StealScript.NewStealableItemRates(battleEnemy.StealableItemRates[2], _v.Caster.Weapon) + (BonusRatioHP / 2) && battleEnemy.StealableItems[2] != RegularItem.NoItem)
+                {
+                    _v.StealItem(battleEnemy, 2);
+                }
+                else if (GameRandom.Next8() < StealScript.NewStealableItemRates(battleEnemy.StealableItemRates[1], _v.Caster.Weapon) + BonusRatioHP && battleEnemy.StealableItems[1] != RegularItem.NoItem)
+                {
+                    _v.StealItem(battleEnemy, 1);
+                }
+                else if (GameRandom.Next8() < StealScript.NewStealableItemRates(battleEnemy.StealableItemRates[0], _v.Caster.Weapon) + BonusRatioHP && battleEnemy.StealableItems[0] != RegularItem.NoItem)
+                {
+                    _v.StealItem(battleEnemy, 0);
+                }
+                return;
+            }
             if (_v.Caster.IsPlayer)
             {
                 _v.WeaponPhysicalParams();
