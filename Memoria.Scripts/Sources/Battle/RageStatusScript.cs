953f8f7bc493ed687da7817ceb7c936cdf15a8fd
DV666 <robincharpentier@hotmail.com>
2025-05-02 18:18:59 +0200

    AbilityFeaturesTS uploaded (backup).

--- Memoria.Scripts/Sources/Battle/RageStatusScript.cs
+++ Memoria.Scripts/Sources/Battle/RageStatusScript.cs
@@ -18,7 +18,7 @@ namespace Memoria.DefaultScripts
         {
             base.Apply(target, inflicter, parameters);
             OverlapSHP.SetupOverlappingSHP2(target);
-            Int32 StackMaximum = 3;
+            Int32 StackMaximum = 5;
             ModelScale = target.ModelStatusScale;
             if (parameters.Length > 0)
             {
