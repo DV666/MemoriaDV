a108e11416eb0daf8a642456cea8940bd7030557
DV666 <robincharpentier@hotmail.com>
2025-06-16 22:18:32 +0200

    Matra Magic better script

--- Memoria.Scripts/Sources/Battle/0027_DirectHPDamageScript.cs
+++ Memoria.Scripts/Sources/Battle/0027_DirectHPDamageScript.cs
@@ -25,50 +25,13 @@ namespace Memoria.Scripts.Battle
             {
                 _v.Context.Attack = (short)(GameRandom.Next16() % (_v.Caster.Magic + _v.Caster.Level));
                 _v.SetCommandPower();
+                _v.Command.Element = (EffectElement)(1 << GameRandom.Next16() % 8);
                 TranceSeekAPI.CasterPenaltyMini(_v);
                 TranceSeekAPI.PenaltyShellAttack(_v);
                 TranceSeekAPI.PenaltyCommandDividedAttack(_v);
                 TranceSeekAPI.BonusElement(_v);
                 if (TranceSeekAPI.CanAttackMagic(_v))
                 {
-                    int num = GameRandom.Next16() % 8;
-                    if (num == 0)
-                    {
-                        _v.Target.CanAttackElement(EffectElement.Cold);
-                    }
-                    else if (num == 1)
-                    {
-                        _v.Target.CanAttackElement(EffectElement.Fire);
-                    }
-                    else if (num == 2)
-                    {
-                        _v.Target.CanAttackElement(EffectElement.Thunder);
-                    }
-                    else if (num == 3)
-                    {
-                        if (_v.Target.IsLevitate)
-                        {
-                            _v.Context.Flags |= BattleCalcFlags.Miss;
-                            return;
-                        }
-                        _v.Target.CanAttackElement(EffectElement.Earth);
-                    }
-                    else if (num == 4)
-                    {
-                        _v.Target.CanAttackElement(EffectElement.Aqua);
-                    }
-                    else if (num == 5)
-                    {
-                        _v.Target.CanAttackElement(EffectElement.Wind);
-                    }
-                    else if (num == 6)
-                    {
-                        _v.Target.CanAttackElement(EffectElement.Holy);
-                    }
-                    else
-                    {
-                        _v.Target.CanAttackElement(EffectElement.Darkness);
-                    }
                     _v.CalcHpDamage();
                 }
                 TranceSeekAPI.TryAlterMagicStatuses(_v);
