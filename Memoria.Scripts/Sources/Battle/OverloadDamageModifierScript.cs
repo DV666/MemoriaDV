9b5ce7a962eb388dd0ce630cd00416bf57b40d16
DV666 <robincharpentier@hotmail.com>
2025-06-21 19:11:10 +0200

    -

--- Memoria.Scripts/Sources/Battle/OverloadDamageModifierScript.cs
+++ Memoria.Scripts/Sources/Battle/OverloadDamageModifierScript.cs
@@ -1,7 +1,12 @@
+using Assets.Sources.Scripts.UI.Common;
+using Memoria.Assets;
 using Memoria.Data;
 using Memoria.Database;
+using Memoria.Prime;
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using static UIManager;
 
 namespace Memoria.Scripts.Battle
 {
@@ -18,26 +23,49 @@ namespace Memoria.Scripts.Battle
 
         public void OnDamageFinalChanges(BattleCalculator v)
         {
-            uint HPMob = btl_para.IsNonDyingVanillaBoss(v.Target) ? (v.Target.CurrentHp - 10000) : v.Target.CurrentHp;
-            if (v.Caster.IsPlayer && HPMob < v.Target.HpDamage && !v.Target.IsPlayer)
+            if (v.Caster.IsPlayer && false)
+                v.Target.HpDamage = 9999;
+
+            uint HPMob = v.Target.CurrentHp;
+            SB2_PATTERN sb2Pattern = FF9StateSystem.Battle.FF9Battle.btl_scene.PatAddr[FF9StateSystem.Battle.FF9Battle.btl_scene.PatNum];
+            if (!v.Target.IsPlayer)
             {
-                int CustomCharacterId = (int)(10000 + v.Caster.Player.PresetId);
+                HPMob = btl_para.IsNonDyingVanillaBoss(v.Target) ? (v.Target.CurrentHp - 10000) : v.Target.CurrentHp;
+                int MonsterId = (int)(10100 + v.Target.Data.bi.line_no);
+                if (v.Target.MaximumHp == UInt16.MaxValue)
+                {
+                    if (!FF9StateSystem.EventState.gScriptDictionary.TryGetValue(MonsterId, out Dictionary<Int32, Int32> dictMonster))
+                    {
+                        BTL_SCENE scene = new BTL_SCENE();
+                        scene.ReadBattleScene(FF9StateSystem.Battle.FF9Battle.btl_scene.nameIdentifier.Replace("BSC_", ""));
+                        SB2_MON_PARM monsterParam = scene.MonAddr[sb2Pattern.Monster[v.Target.Data.bi.slot_no].TypeNo];
+                        dictMonster = new Dictionary<Int32, Int32>();
+                        dictMonster[0] = (int)monsterParam.MaxHP; // True Maximum HP
+                        dictMonster[1] = (int)monsterParam.MaxHP; // True Current HP
+                        FF9StateSystem.EventState.gScriptDictionary.Add(MonsterId, dictMonster);
+                    }
+                    HPMob = (uint)dictMonster[1];
+                    dictMonster[1] = (v.Target.Flags & CalcFlag.HpRecovery) != 0 ? Math.Min(dictMonster[0], dictMonster[1] + v.Target.HpDamage) : Math.Max(0, dictMonster[1] - v.Target.HpDamage);
+                }
+            }
+            if (v.Caster.IsPlayer && (!v.Target.IsPlayer && HPMob <= v.Target.HpDamage && (v.Target.Flags & CalcFlag.HpRecovery) == 0) || (v.Target.IsPlayer && (v.Target.Flags & CalcFlag.HpRecovery) != 0 && v.Target.Data != v.Caster.Data && v.Caster.IsNonMorphedPlayer))
+            {
+                int CustomCharacterId = (int)(10000 + v.Caster.PlayerIndex);
                 if (FF9StateSystem.EventState.gScriptDictionary.TryGetValue(CustomCharacterId, out Dictionary<Int32, Int32> dict))
                 {
                     FF9BattleDB.SceneData.TryGetKey(FF9StateSystem.Battle.battleMapIndex, out string btlName);
-                    SB2_PATTERN sb2Pattern = FF9StateSystem.Battle.FF9Battle.btl_scene.PatAddr[FF9StateSystem.Battle.FF9Battle.btl_scene.PatNum];
                     btlName = FF9StateSystem.Battle.FF9Battle.btl_scene.nameIdentifier.Replace("BSC_", "");
                     CharacterPresetId presetId = v.Caster.Player.PresetId;
                     v.Caster.AddDelayedModifier(
                         caster => caster.CurrentAtb >= caster.MaximumAtb,
                         caster =>
                         {
-                            v.Caster.ChangeToMonster(btlName, sb2Pattern.Monster[v.Target.Data.bi.slot_no].TypeNo, CharacterCommands.CommandSets[presetId].Regular[2], (BattleCommandId)1111, false, true, true, true, true);
+                            BattleCommandId CMDMonster = (BattleCommandId)(5000 + v.Caster.Data.bi.line_no);
+                            v.Caster.ChangeToMonster(btlName, sb2Pattern.Monster[v.Target.Data.bi.slot_no].TypeNo, CharacterCommands.CommandSets[presetId].Regular[2], CMDMonster, false, true, true, true, true, AADescription: true, updateStatus:true);
                             CharacterCommands.CommandSets[presetId].Regular[3] = BattleCommandId.None;
-                            if (btl_para.IsNonDyingVanillaBoss(v.Target))
+                            if ((btl_para.IsNonDyingVanillaBoss(v.Target) || v.Caster.Data.dms_geo_id == 347 || v.Caster.Data.dms_geo_id == 125) && v.Caster.Data.dms_geo_id != 349 && v.Caster.Data.dms_geo_id != 300 && v.Caster.Data.dms_geo_id != 302)
                             {
                                 v.Caster.MaximumHp -= 10000;
-                                v.Caster.MaximumMp -= 10000;
                                 v.Caster.CurrentHp = Math.Max(1, v.Target.MaximumHp);
                                 v.Caster.CurrentMp = Math.Max(1, v.Target.MaximumMp);
                                 dict[2] = 1;
@@ -49,11 +77,26 @@ namespace Memoria.Scripts.Battle
                                 dict[2] = 0;
                             }
                             dict[0] = FF9StateSystem.Battle.battleMapIndex;
-                            dict[1] = sb2Pattern.Monster[v.Target.Data.bi.slot_no].TypeNo;
+                            if (v.Caster.Data.dms_geo_id == 339)
+                                dict[1] = 0;
+                            else
+                                dict[1] = sb2Pattern.Monster[v.Target.Data.bi.slot_no].TypeNo;
+                            Dictionary<Int32, String> fieldText = new Dictionary<Int32, String>();
+                            String path = EmbadedTextResources.GetCurrentPath("/Battle/" + dict[0] + ".mes");
+                            FF9TextTool.ImportStrtWithCumulativeModFiles<Int32>(path, fieldText);
+                            caster.Player.Name = Regex.Replace(fieldText[dict[1]], @"\[.*?\]", ""); ;
                         }
                     );
                 }
             }
+
+            if (v.Target.Flags == 0)
+                return;
+            Int32 reflectMultiplier = v.Command.GetReflectMultiplierOnTarget(v.Target.Id);
+            if ((v.Target.Flags & CalcFlag.HpAlteration) != 0)
+                v.Target.HpDamage *= reflectMultiplier;
+            if ((v.Target.Flags & CalcFlag.MpAlteration) != 0)
+                v.Target.MpDamage *= reflectMultiplier;
         }
     }
 }
--- Memoria.Scripts/Sources/Battle/OverloadOnBattleInitScript.cs
+++ Memoria.Scripts/Sources/Battle/OverloadOnBattleInitScript.cs
@@ -1,7 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Assets.Sources.Scripts.UI.Common;
+using FF9;
+using Memoria.Assets;
 using Memoria.Data;
 using Memoria.Database;
+using Memoria.Prime;
+using static BTL_DATA;
+using static UIManager;
 
 namespace Memoria.Scripts.Battle
 {
@@ -12,31 +19,37 @@ namespace Memoria.Scripts.Battle
 
         public void OnBattleInit()
         {
+            // Reset
+            FF9StateSystem.EventState.gScriptDictionary.Remove(10104);
+            FF9StateSystem.EventState.gScriptDictionary.Remove(10105);
+            FF9StateSystem.EventState.gScriptDictionary.Remove(10106);
+            FF9StateSystem.EventState.gScriptDictionary.Remove(10107);
+
             foreach (BattleUnit PlayerUnit in BattleState.EnumerateUnits())
             {
                 if (!PlayerUnit.IsPlayer)
                     continue;
 
-                int CustomCharacterId = (int)(10000 + PlayerUnit.Player.PresetId);
+                int CustomCharacterId = (int)(10000 + PlayerUnit.PlayerIndex);
                 if (!FF9StateSystem.EventState.gScriptDictionary.TryGetValue(CustomCharacterId, out Dictionary<Int32, Int32> dict))
                 {
                     dict = new Dictionary<Int32, Int32>();
-                    dict[0] = -1; // Scene
+                    dict[0] = -1; // ID Battle (for Scene) 
                     dict[1] = -1; // ID Monster 
                     dict[2] = -1; // Boss ?
                     FF9StateSystem.EventState.gScriptDictionary.Add(CustomCharacterId, dict);
                 }
                 if (dict[0] != -1 && dict[1] != -1)
                 {
+                    BattleCommandId CMDMonster = (BattleCommandId)(5000 + PlayerUnit.Data.bi.line_no);
                     FF9BattleDB.SceneData.TryGetKey(dict[0], out string btlName);
                     btlName = btlName.Replace("BSC_", "");
                     CharacterPresetId presetId = PlayerUnit.Player.PresetId;
-                    PlayerUnit.ChangeToMonster(btlName, dict[1], CharacterCommands.CommandSets[presetId].Regular[2], (BattleCommandId)1111, false, true, true, true, true);
+                    PlayerUnit.ChangeToMonster(btlName, dict[1], CharacterCommands.CommandSets[presetId].Regular[2], CMDMonster, false, true, true, true, true, AADescription: true, updateStatus:true);
                     CharacterCommands.CommandSets[presetId].Regular[3] = BattleCommandId.None;
-                    if (dict[2] > 0)
+                    if (dict[2] > 0 || PlayerUnit.Data.dms_geo_id == 347 || PlayerUnit.Data.dms_geo_id == 125)
                     {
                         PlayerUnit.MaximumHp -= 10000;
-                        PlayerUnit.MaximumMp -= 10000;
                         PlayerUnit.CurrentHp = Math.Max(1, PlayerUnit.MaximumHp);
                         PlayerUnit.CurrentMp = Math.Max(1, PlayerUnit.MaximumMp);
                     }
@@ -45,9 +58,38 @@ namespace Memoria.Scripts.Battle
                         PlayerUnit.CurrentHp = PlayerUnit.MaximumHp;
                         PlayerUnit.CurrentMp = PlayerUnit.MaximumMp;
                     }
+                    if (PlayerUnit.Data.dms_geo_id == 427) // Beatrix (Stock Break & ClimHazard nerfed)
+                    {
+                        if (dict[0] == 4)
+                        {
+                            PlayerUnit.Data.monster_transform.spell[4].Ref.ScriptId = 100;
+                            PlayerUnit.Data.monster_transform.spell[4].Ref.Power = 28;
+                            PlayerUnit.Data.monster_transform.spell[4].Ref.Rate = 0;
+                        }
+                        else if (dict[0] == 299)
+                        {
+                            PlayerUnit.Data.monster_transform.spell[4].Ref.ScriptId = 100;
+                            PlayerUnit.Data.monster_transform.spell[4].Ref.Power = 47;
+                            PlayerUnit.Data.monster_transform.spell[4].Ref.Rate = 0;
+                        }
+                        else if (dict[0] == 73)
+                        {
+                            PlayerUnit.Data.monster_transform.spell[5].Ref.ScriptId = 100;
+                            PlayerUnit.Data.monster_transform.spell[5].Ref.Power = 50;
+                            PlayerUnit.Data.monster_transform.spell[5].Ref.Rate = 0;
+                        }
+                        string Description = "";
+                        int Power = dict[0] == 73 ? PlayerUnit.Data.monster_transform.spell[5].Ref.Power : PlayerUnit.Data.monster_transform.spell[4].Ref.Power;
+                        Description += "[ICON=95] ";
+                        Description += $"{Localization.Get("AttackStats")} : {Power}";
+                        PlayerUnit.Data.monster_transform.spell_desc[4] = Description;
+                    }
+                    Dictionary<Int32, String> fieldText = new Dictionary<Int32, String>();
+                    String path = EmbadedTextResources.GetCurrentPath("/Battle/" + dict[0] + ".mes");
+                    FF9TextTool.ImportStrtWithCumulativeModFiles<Int32>(path, fieldText);
+                    PlayerUnit.Player.Name = Regex.Replace(fieldText[dict[1]], @"\[.*?\]", ""); ;
                 }
             }
-
         }
     }
 }
